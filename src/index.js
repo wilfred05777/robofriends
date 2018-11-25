3044ba1bc646f6cfb118bfdf6f45fd1f582d214d
WilfredErdo <wilfred05777@gmail.com>
2018-11-25 11:16:57 +0800

    Adding RoboFriends Project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+// import {robots} from './robots';
+// import CardList from './CardList';
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+serviceWorker.unregister();
